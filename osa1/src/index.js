a560a0eed59c0fbd02d7ffd6687ab8386d662a29
Mika Le Gall <mika.legall@live.fi>
2018-04-12 15:15:57 +0300

    talteen

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -3,74 +3,40 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
     
-    const kurssi = {
-        nimi: 'Half Stack -sovelluskehitys',
-        osat: [
-            {
-            nimi: 'Reactin perusteet',
-            tehtavia: 10
-            },
-
-            {
-            nimi: 'Tiedonvälitys propseilla',
-            tehtavia: 7
-            },
-
-            {
-            nimi: 'Komponenttien tila',
-            tehtavia: 14
-            }
-        ]
-    } 
+  
 
     return (
       <div>
-        <Otsikko kurssi={kurssi} />
-
-        <Sisalto kurssi={kurssi} />
-
-        <Yhteensa kurssi={kurssi} />
+      <b>anna palautetta</b>
+      
+      <br />
+      <b>statistiikka</b>
       </div>
     )
   }
 
-
-const Otsikko = (props) =>{
-    return (
-    <div>
-        <p><b>{props.kurssi.nimi}</b></p>
-    </div>
-    )
-}
-
-const Sisalto = (props) =>{
-      return (
-    <div>
-        <Osa osa={props.kurssi.osat[0].nimi} tehtavia={props.kurssi.osat[0].tehtavia} />
-        <Osa osa={props.kurssi.osat[1].nimi} tehtavia={props.kurssi.osat[1].tehtavia} />
-        <Osa osa={props.kurssi.osat[2].nimi} tehtavia={props.kurssi.osat[2].tehtavia} />
-    </div>
-    )
-}
-
-const Yhteensa = (props) =>{
-    return (
-    <div>
-        <p>Yhteensä {props.kurssi.osat[0].tehtavia + props.kurssi.osat[1].tehtavia + props.kurssi.osat[2].tehtavia} tehtävää</p>
-    </div>
-    )
-}
-
-const Osa = (props) =>{
-    return (
-    <div>
-        <p>{props.osa} {props.tehtavia}</p>
-    </div>
-    )
-}
 
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 )
+
+
+this.setState(
+
+    {
+    
+                                  [arvostelu]: this.state[arvostelu] + 1,
+    
+                                  keskiarvo: (keskiarvo + arvo) / (yhteensa + 1),
+    
+                                  yhteensa: yhteensa + 1
+    
+                   }
+    
+    )
+    
+     
+    
+    jossa arvostelu on string tyyppiä ‘hyvia’ tms.
