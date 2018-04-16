e12e4b422ab4f0a7c2fc0185810a6ca556ce3c5d
Mika Le Gall <mika.legall@live.fi>
2018-04-16 16:40:54 +0300

    säilöön

---
+++ osa1/src/1.7.index_kesken.js
@@ -0,0 +1,121 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+class App extends React.Component {
+    constructor() {
+      super()
+      this.state = {
+        laskuriHyvä: 0,
+        laskuriNeutraali: 0,
+        laskuriHuono: 0
+      }
+    }
+  
+    render() {
+      return (
+        <div>
+          <h2>anna palautetta</h2>
+          <p>
+          <button onClick={() => this.setState({ laskuriHyvä: this.state.laskuriHyvä + 1 }) & console.log('Klikattiin "Hyvä"')}>
+            hyvä
+          </button>
+          <button onClick={() => this.setState({ laskuriNeutraali: this.state.laskuriNeutraali + 1 }) & console.log('Klikattiin "Neutraali"')}>
+            neutraali
+          </button>
+          <button onClick={() => this.setState({ laskuriHuono: this.state.laskuriHuono + 1 }) & console.log('Klikattiin "Huono"')}>
+            huono
+          </button>
+          </p>
+          <h2>statistiikka</h2>
+          <p>
+            Hyvä {this.state.laskuriHyvä}
+            <br />
+            Neutraali {this.state.laskuriNeutraali}
+            <br />
+            Huono {this.state.laskuriHuono}
+            <br />
+            Keskiarvo { (this.state.laskuriHyvä - this.state.laskuriHuono) / (this.state.laskuriHyvä + this.state.laskuriNeutraali + this.state.laskuriHuono) }
+            <br />
+            Positiivisia { Math.round((this.state.laskuriHyvä * 100 / (this.state.laskuriHyvä + this.state.laskuriNeutraali + this.state.laskuriHuono)),1) } %
+        </p>
+        </div>
+      )
+    }
+  }
+  
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  )
+
+
+/*
+class App extends React.PureComponent {
+    constructor(props) {
+      super(props)
+      this.state = {
+        counter: 1,
+        nimi: 'Lare'
+      }
+    
+  
+    setInterval(() => {
+        this.setState({ counter: this.state.counter + 1 })
+      }, 1000)
+    }
+
+    render() {
+        console.log('renderöidään', this.state.counter)
+        return (
+        <div>
+            {this.state.counter}
+            <br />
+                {this.state.nimi}
+        </div>
+      )
+    }
+  }
+  
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  )
+*/
+
+
+/*
+const App = (props) => {
+    
+    var {kapseliPropsi} = props
+  
+
+    return (
+      <div>
+      <b>anna palautetta</b>
+      
+      <br />
+      <b>statistiikka</b>
+      <div>{kapseliPropsi.arvo}</div>
+      </div>
+    )
+  }
+
+  const laskuriMuuttuja = {
+    arvo: 1
+  }
+
+  const renderoi = () => {
+    ReactDOM.render(
+      <App kapseliPropsi={laskuriMuuttuja} />,
+      document.getElementById('root')
+    )
+  }
+  
+  setInterval(() => {
+    renderoi()
+    laskuriMuuttuja.arvo += 1;
+  }, 1000)
+
+  */
+
--- osa1/src/index.js
+++ osa1/src/index.js
@@ -2,116 +2,64 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-class App extends React.Component {
+/*const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}*/
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+class App extends React.PureComponent {
     constructor() {
       super()
       this.state = {
         laskuriHyvä: 0,
         laskuriNeutraali: 0,
-        laskuriHuono: 0
+        laskuriHuono: 0,
+        counter: 1
       }
     }
-  
+    
+
+    asetaArvoon = (arvo) => {
+      return () => {
+        this.setState({ laskuriHyvä: arvo })
+      }
+    }
+
+    
     render() {
       return (
         <div>
-          <h2>anna palautetta</h2>
-          <p>
-          <button onClick={() => this.setState({ laskuriHyvä: this.state.laskuriHyvä + 1 }) & console.log('Klikattiin "Hyvä"')}>
-            hyvä
-          </button>
-          <button onClick={() => this.setState({ laskuriNeutraali: this.state.laskuriNeutraali + 1 }) & console.log('Klikattiin "Neutraali"')}>
-            neutraali
-          </button>
-          <button onClick={() => this.setState({ laskuriHuono: this.state.laskuriHuono + 1 }) & console.log('Klikattiin "Huono"')}>
-            huono
-          </button>
-          </p>
-          <h2>statistiikka</h2>
-          <p>
-            Hyvä {this.state.laskuriHyvä}
-            <br />
-            Neutraali {this.state.laskuriNeutraali}
-            <br />
-            Huono {this.state.laskuriHuono}
-        </p>
+          <Display counter={this.state.counter}/>
+          <div>
+            <Button
+              handleClick={this.asetaArvoon(this.state.counter + 1)}
+              text="Plus"
+            />
+            <Button
+              handleClick={this.asetaArvoon(this.state.counter - 1)}
+              text="Minus"
+            />
+            <Button
+              handleClick={this.asetaArvoon(0)}
+              text="Zero"
+            />
+          </div>
         </div>
       )
     }
   }
+
   
   ReactDOM.render(
     <App />,
     document.getElementById('root')
   )
-
-
-/*
-class App extends React.PureComponent {
-    constructor(props) {
-      super(props)
-      this.state = {
-        counter: 1,
-        nimi: 'Lare'
-      }
-    
-  
-    setInterval(() => {
-        this.setState({ counter: this.state.counter + 1 })
-      }, 1000)
-    }
-
-    render() {
-        console.log('renderöidään', this.state.counter)
-        return (
-        <div>
-            {this.state.counter}
-            <br />
-                {this.state.nimi}
-        </div>
-      )
-    }
-  }
-  
-  ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-  )
-*/
-
-
-/*
-const App = (props) => {
-    
-    var {kapseliPropsi} = props
-  
-
-    return (
-      <div>
-      <b>anna palautetta</b>
-      
-      <br />
-      <b>statistiikka</b>
-      <div>{kapseliPropsi.arvo}</div>
-      </div>
-    )
-  }
-
-  const laskuriMuuttuja = {
-    arvo: 1
-  }
-
-  const renderoi = () => {
-    ReactDOM.render(
-      <App kapseliPropsi={laskuriMuuttuja} />,
-      document.getElementById('root')
-    )
-  }
-  
-  setInterval(() => {
-    renderoi()
-    laskuriMuuttuja.arvo += 1;
-  }, 1000)
-
-  */
-
