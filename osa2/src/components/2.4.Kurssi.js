94b48b9c32c66a5da782db197df4355efece6044
Mika Le Gall <mika.legall@live.fi>
2018-04-20 13:06:18 +0300

    2.4. ja 2.5.

---
+++ osa2/src/components/2.4.Kurssi.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Sisalto from './Sisalto'
+
+const Kurssi = ({ props }) => {
+    console.log('Kurssi props.osat on:  ', props.osat)
+    
+let yhteensäSumma = props.osat.reduce(
+    function
+    (summa, tehtavatPerOsa){
+        return summa + tehtavatPerOsa.tehtavia
+    }, 0 // "0" = "summa" (tämä alustus on pakollinen laskennan aloituspisteeksi) https://youtu.be/Wl98eZpkp-c?t=5m38s
+)    
+
+
+    return (
+        <div>
+            <b>{props.nimi}</b>
+                <Sisalto props={props.osat}/>
+                yhteensä {yhteensäSumma} tehtävää
+            <br />
+            <br />
+      </div>
+    )
+  }
+
+export default Kurssi
