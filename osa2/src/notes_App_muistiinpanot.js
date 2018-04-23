29fd763980c839c166b6ec5a4e2d098332f5a6b3
Mika Le Gall <mika.legall@live.fi>
2018-04-23 10:11:58 +0300

    Näytä-piilota toiminto

--- osa2/src/notes_App_muistiinpanot.js
+++ osa2/src/notes_App_muistiinpanot.js
@@ -78,43 +78,49 @@ toggleVisible = () => {
 export default App
 
 
-/*
-import React from 'react';
+/* index.js
 
-class App extends React.Component {
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-      persons: [
-        { name: 'Arto Hellas' }
-      ],
-      newName: ''
-    }
+const notes = [
+  {
+    id: 1,
+    content: 'HTML on helppoa',
+    date: '2017-12-10T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Selain pystyy suorittamaan vain javascriptiä',
+    date: '2017-12-10T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'HTTP-protokollan tärkeimmät metodit ovat GET ja POST',
+    date: '2017-12-10T19:20:14.298Z',
+    important: true
   }
+]
 
-  render() {
-    return (
-      <div>
-        <h2>Puhelinluettelo</h2>
-        <form>
-          <div>
-            nimi: <input />
-          </div>
-          <div>
-            debug: {this.state.newName}
-          </div>
-          <div>
-            <button type="submit">lisää</button>
-          </div>
-        </form>
-        <h2>Numerot</h2>
-        foo
-      </div>
-    )
-  }
+ReactDOM.render(
+  <App notes={notes} />,
+  document.getElementById('root')
+)
+
+*/
+
+
+/* Note.js
+import React from 'react'
+
+const Note = ({ note }) => {
+  return (
+    <li>{note.content}</li>
+  )
 }
 
-export default App
+export default Note
 */
