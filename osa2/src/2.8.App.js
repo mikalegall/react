9e47b28acb953d4de2f8741cbb45302c038b3f26
Mika Le Gall <mika.legall@live.fi>
2018-04-24 12:05:13 +0300

    2.8.

---
+++ osa2/src/2.8.App.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import Henkilo from './components/Henkilo'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      henkilot: [
+        { nimi: 'Arto Hellas',
+          numero: '040-123456' }
+      ],
+      uusiNimi: '',
+      uusiNumero: ''
+    }
+  }
+
+
+  
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+kasitteleMuutosNimi = (event) => {
+    // event.target vastaa kontrolloidun syötekomponentin input-kenttää
+      //  event.target.value vastaa kontrolloidun syötekomponentin kentän arvoa
+        console.log('kasitteleMuutosNimi --> event.target.value on: ', event.target.value)
+          this.setState({ uusiNimi: event.target.value })
+  }
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+kasitteleMuutosNumero = (event) => {
+  // event.target vastaa kontrolloidun syötekomponentin input-kenttää
+    //  event.target.value vastaa kontrolloidun syötekomponentin kentän arvoa
+      console.log('kasitteleMuutosNumero --> event.target.value on: ', event.target.value)
+        this.setState({ uusiNumero: event.target.value })
+}
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+
+// ***********************************************************************************************************
+  // Lomakkeen kentän kautta vastaanotetaan muutos, joka halutaan tallentaa
+  lisaaMuutos = (event) => {
+    event.preventDefault()
+        console.log('this.state.uusiNimi = ', this.state.uusiNimi)
+
+
+        // 2.7. JÄIN TÄHÄN!! IF 'this.state.henkilot.nimi' (luuppaa läpi) === this.state.uusiNimi NIIN window.alert('Henkilö on jo tallennettu')
+        // https://react-cn.github.io/react/tips/if-else-in-JSX.html
+        // Jostain syystä .indexOf() antaa aina vastaukseksi -1
+        // console.log('INDEX tarkistus = ', this.state.henkilot.indexOf(this.state.uusiNimi))
+        let ehdokas = this.state.uusiNimi
+        this.state.henkilot.forEach(function(taulukonAlkio) {
+                                            console.log('PÖÖ = ', taulukonAlkio.nimi)
+          if (taulukonAlkio.nimi === ehdokas) {
+            window.alert('Henkilö on jo tallennettu')
+        }
+        }) 
+        // TODO: IF ehdon täyttyessä hyppää pois tästä 'lisaaMuutos()' -funktiometodista eli keskeytä alla olevien rivien suorittaminen
+       
+          
+        
+
+       const olioJSON = {
+        nimi: this.state.uusiNimi,
+        numero: this.state.uusiNumero
+      }
+
+  const henkilot = this.state.henkilot.concat(olioJSON)
+      this.setState({ // // Lopulta komponentin Tila (state) päivitetään
+        henkilot, // uudella muistiinpanolla ja
+        uusiNimi: '', // tyhjennetään kontrolloidun syötekomponentin kenttä
+        uusiNumero: ''  // tyhjennetään kontrolloidun syötekomponentin kenttä
+      })
+
+
+
+  }
+// ***********************************************************************************************************
+  
+
+
+
+
+
+render() {
+
+    return (
+      <div>
+        <h2>Puhelinluettelo</h2>
+
+        <form onSubmit={this.lisaaMuutos}>
+          <div>
+            nimi:
+
+            <input 
+            value={this.state.uusiNimi} 
+            // Tapahtumankäsittelijää kutsutaan aina kun syötekomponentissa tapahtuu jotain 
+            onChange={this.kasitteleMuutosNimi}
+          />
+
+          </div>
+          <div>
+            numero:
+
+            <input 
+            value={this.state.uusiNumero} 
+            // Tapahtumankäsittelijää kutsutaan aina kun syötekomponentissa tapahtuu jotain 
+            onChange={this.kasitteleMuutosNumero}
+          />
+
+          </div>
+          <div>
+            <button type="submit">lisää</button>
+          </div>
+        </form>
+        <h2>Numerot</h2>
+
+        {/* Yhden henkilön näyttäminen olisi 
+              <Henkilo key={this.state.henkilot[0].nimi} props={this.state.henkilot[0]} />
+        */}
+        {console.log('this.state.henkilot[0].nimi = ', this.state.henkilot[0].nimi)}
+        {// Järjestetään taulukon sisältämä lista uudelleen map-function avulla (näytetään kaikki taulukon sisältämät JSON-oliot)
+        }
+        {this.state.henkilot.map(hlo =>  
+        <Henkilo key={hlo.nimi} props={hlo} />
+        )}
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ osa2/src/components/2.8.Henkilo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Henkilo = ({ props }) => {
+  return (
+  
+    <div>{props.nimi} {props.numero}</div>
+  )
+}
+
+export default Henkilo
