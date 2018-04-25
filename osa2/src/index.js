63086da7cd44936b09b9c24e32ba00fd3101aab9
Mika Le Gall <mika.legall@live.fi>
2018-04-25 10:00:24 +0300

    talteen

--- osa2/src/index.js
+++ osa2/src/index.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import axios from 'axios'
 
-const promise = axios.get('http://localhost:3001/notes')
-console.log(promise)
-
-const promise2 = axios.get('http://localhost:3001/foobar')
-console.log(promise2)
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 )
+
+/* import axios from 'axios'
+axios.get('http://localhost:3001/notes')
+     .then(response =>
+        {
+          const notes = response.data
+  
+            ReactDOM.render(
+              <App notes={notes} />,
+                document.getElementById('root')
+            )
+        }
+      )
+*/
